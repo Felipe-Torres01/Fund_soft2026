262a86984e78928140980978d5229732f2f8911d
Felipe-Torres01 <torres.felipe@edu.pucrs.br>
2026-09-16 21:20:49 +0000

    ultimas aulas

--- ex1-biblioteca-v4-solRotas/ex1-biblioteca-v4-solRotas/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
+++ ex1-biblioteca-v4-solRotas/ex1-biblioteca-v4-solRotas/src/main/java/com/bcopstein/ex1biblioeca/Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    
+    private List<Livro> livros;
+
+    public Controller() {
+        livros = new LinkedList<>();
+        livros.add(new Livro(100, "Aprendendo Spring-Boot", "Huguinho Pato", 2020));
+        livros.add(new Livro(120, "Aprendendo Java", "Zezinho Pato", 2015));
+        livros.add(new Livro(140, "Aprendendo Outra coisa", "Luizinho Pato", 2023));
+        livros.add(new Livro(140, "Aprendendo Uma coisa nova", "Huguinho Pato", 2023));
+        livros.add(new Livro(140, "Aprendendo Outra coisa nova", "Huguinho Pato", 2023));
+    }
 
     @GetMapping("")
     @CrossOrigin(origins = "*")
@@ -38,7 +48,7 @@ public class Controller {
                 .toList();
     }
 
-    @GetMapping("livrosautor")// livrosautor?autor=Huguinho Pato
+    @GetMapping("livrosautor") // livrosautor?autor=Huguinho Pato
     @CrossOrigin(origins = "*")
     public List<Livro> getLivrosDoAutor(@RequestParam(value = "autor") String autor) {
         return livros.stream()
@@ -53,7 +63,7 @@ public class Controller {
         return true;
     }
 
-    @GetMapping("livrosporano")// livrosporano?ano=2023
+    @GetMapping("livrosporano") // livrosporano?ano=2023
     @CrossOrigin(origins = "*")
     public List<Livro> getLivrosPorAno(@RequestParam(value = "ano") int ano) {
         return livros.stream()
@@ -61,17 +71,18 @@ public class Controller {
                 .toList();
     }
 
-    @GetMapping("/desatualizados/{ano}") //desatualizados/2023
+    @GetMapping("/desatualizados/{ano}") // desatualizados/2023
     @CrossOrigin(origins = "*")
-    public List<Livro> getLivrosDoAutor(@PathVariable(value="ano")int ano) {
+    public List<Livro> getLivrosDoAutorAnterior(@PathVariable(value = "ano") int ano) {
         return livros.stream()
                 .filter(livro -> livro.getAno() < ano)
                 .toList();
     }
 
-    @GetMapping("/livrosautor/{autor}/ano/{ano}") //livrosautor/Huguinho Pato/ano/2023
+    @GetMapping("/livrosautor/{autor}/ano/{ano}") // livrosautor/Huguinho Pato/ano/2023
     @CrossOrigin(origins = "*")
-    public List<Livro> getLivrosDoAutor(@PathVariable(value="autor") String autor, @PathVariable(value="ano")int ano) {
+    public List<Livro> getLivrosDoAutorAnual(@PathVariable(value = "autor") String autor,
+            @PathVariable(value = "ano") int ano) {
         return livros.stream()
                 .filter(livro -> livro.getAutor().equals(autor.trim()))
                 .filter(livro -> livro.getAno() == ano)
@@ -82,12 +93,36 @@ public class Controller {
     @CrossOrigin(origins = "*")
     public boolean atualizaLivro(@RequestBody final Livro livroAtualizado) {
         Optional<Livro> livroOp = livros.stream()
-            .filter(l->l.getId() == livroAtualizado.getId())
-            .findAny();
-        if (livroOp.isPresent()){
+                .filter(l -> l.getId() == livroAtualizado.getId())
+                .findAny();
+        if (livroOp.isPresent()) {
             livros.remove(livroOp.get());
             livros.add(livroAtualizado);
         }
         return true;
     }
+
+    @GetMapping("/livro/{id}")
+    public Livro getLivroPorId(@PathVariable int id) {
+        Livro livroEncontrado = livros.stream() // cria uma variavel e tranf em fluxo
+                .filter(livro -> livro.getId() == id)// filtra
+                .findAny()// pega o primeiro
+                .orElse(null);// se nao achou return null
+        return livroEncontrado;
+    }
+
+    @DeleteMapping("/livro/{id}")
+    public String removeLivroPorId(@PathVariable int id) {
+        Livro livroDeletado = livros.stream()
+                .filter(livro -> livro.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if (livroDeletado != null) {
+            livros.remove(livroDeletado);
+            return ("livro deletado");
+        }
+        return ("livro não encontrado");
+    }
+
 }
